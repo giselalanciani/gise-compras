15aa6b7c4ee070a3c0078ee28268ff73f8890a16
Gisela Lanciani <giselalanciani@gmail.com>
2023-03-08 12:07:21 -0300

    adding components

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Product } from '../products';
+import { ActivatedRoute } from '@angular/router';
+import { CartService } from '../cart.service';
+import { Product, products } from '../products';
 
 @Component({
   selector: 'app-product-detail',
@@ -7,11 +9,25 @@ import { Product } from '../products';
   styleUrls: ['./product-detail.component.scss']
 })
 export class ProductDetailComponent implements OnInit {
-  
-  ngOnInit(): void {
-    
-  }
   product: Product | undefined;
-
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+  ) {}
   
+  ngOnInit() {
+    // First get the product id from the current route.
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+
+    // Find the product that correspond with the id provided in route.
+    this.product = products.find(
+      (product) => product.id === productIdFromRoute
+    );
+  }
+
+  addToCart(product: Product) {
+    this.cartService.addProduct(product);
+    alert('product added to your cart!')
+  }
 }
