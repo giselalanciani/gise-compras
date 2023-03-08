65fbdff0a1e3aed9c91645afdb63c8b691bd10cf
Gisela Lanciani <giselalanciani@gmail.com>
2023-03-08 11:30:08 -0300

    create ng angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatIconModule } from '@angular/material/icon';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
@@ -16,7 +16,8 @@ import { ProductDetailComponent } from './product-detail/product-detail.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    MatIconModule,   
   ],
   providers: [],
   bootstrap: [AppComponent]
